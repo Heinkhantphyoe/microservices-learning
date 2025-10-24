fe493922445d0d1af7f205a3bc4b66becaa23a89
Hein Khant Phyoe <heinkhantphyoe2811@gmail.com>
2025-10-24 18:10:22 +0800

    added for docker container

--- currency-exchange-service/currency-exchange-service/src/main/java/com/hkp/microservices/currency_exchange_service/controller/CurrencyExchangeController.java
+++ currency-exchange-service/currency-exchange-service/src/main/java/com/hkp/microservices/currency_exchange_service/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.hkp.microservices.currency_exchange_service.controller;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,10 +21,14 @@ public class CurrencyExchangeController {
 	
 	@Autowired
 	CurrencyExchangeRepository currencyExchangeRepository;
+	
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(@PathVariable String from,
 			                                      @PathVariable String to) {
+		logger.info("retrieve exchange call with {} to {}", from , to);
+		
 		CurrencyExchange currencyExchange = this.currencyExchangeRepository.findByFromAndTo(from, to);
 		if (currencyExchange == null) {
 			throw new RuntimeException("Unable to Find data for " + from + " to " + to);
